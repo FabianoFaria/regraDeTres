d052d1941e40d2bb5b160f0ba36e5861f53b8833
FabianoFaria <fabiano_hatori@yahoo.com.br>
2016-07-24 22:16:16 -0300

    24072016 - implementacao da index

--- index.php
+++ index.php
@@ -1,13 +1,121 @@
 <html>
 	<head>
-
+		<meta charset="UTF-8">
 		<title> Regra de 3 fácil</title>
+
+
+		<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+		<link rel="stylesheet" href="css/regra_tres.css" type="text/css">
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+
 
 	</head>
 	<body>
 
+		<div class="container">
+      		<div class="header clearfix">
+      			<nav>
+		          <ul class="nav nav-pills pull-right">
+		            <li role="presentation" class="active"><a href="#">Home</a></li>
+		            <li role="presentation"><a href="#">About</a></li>
+		            <li role="presentation"><a href="#">Contact</a></li>
+		          </ul>
+		        </nav>
+		        <h3 class="text-muted">Regra de 3 fácil</h3>
+      		</div>
 
-		<h2>Teste</h2>
+      		<div class="jumbotron">
+		        <h1>Projeto regra de três</h1>
+		        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+		        <p></p>
+		    </div>
 
+		    
+			<div class="row">
+		        <div class="col-lg-12">
+		          
+		        	<form id="formRegraTres" method="post" action="#">
+
+
+		        		<div class="row">
+
+		        			<div class="col-md-4">
+			        			<label >Teste
+	 							
+	 								<input type="text" name="numeroX" >
+
+		 						<label>
+		 					</div>
+
+		 					<div class="col-md-4">
+
+		 						<label> Teste
+
+		 							<input type="text" name="numeroX" >
+
+	 							</label>
+
+		 					</div>
+
+	 						<div class="col-md-4">
+
+	 							<label >Teste
+	 							
+
+		 						<label>
+
+	 						</div>					
+
+		        		</div>
+
+		        		<div class="row">
+
+
+		        			<div class="col-md-4">
+			        			<label >Teste
+	 							
+	 								<input type="text" name="numeroX" >
+
+		 						<label>
+		 					</div>
+
+		 					<div class="col-md-4">
+
+		 						<label> Teste
+
+		 							<input type="text" name="numeroX" >
+
+	 							</label>
+
+		 					</div>
+
+	 						<div class="col-md-4">
+
+	 							<label >Teste
+	 							
+
+		 						<label>
+
+	 						</div>	
+
+
+		        		</div>
+		        		
+
+		        	</form>
+
+		        </div>
+
+		    </div>
+
+      <footer class="footer">
+        <p>&copy; Regra de três - 2016</p>
+      </footer>
+
+
+
+
+      	</div>
 	</body>
 </html>
