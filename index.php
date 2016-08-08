c981651b7fe6c86df9b8aaa03253443467c6e36a
FabianoFaria <fabiano_hatori@yahoo.com.br>
2016-08-08 00:39:10 -0300

    08082016 - Melhorias no formulario do formulario

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
 		<meta charset="UTF-8">
 		<title> Regra de 3 fácil</title>
 
-
-		<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
 		<link rel="stylesheet" href="css/regra_tres.css" type="text/css">
+		<link rel="stylesheet" href="css/bootstrap.css" type="text/css">
+		<link rel="stylesheet" href="css/bootstrap-switch.min.css" type="text/css">
+		
 		<script type="text/javascript" src="js/jquery.js"></script>
 		<script type="text/javascript" src="js/jquery-ui-1.10.3.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.js"></script>
+		<script type="text/javascript" src="js/bootstrap-switch.min.js"></script>	
 		<script type="text/javascript" src="js/plugins.js"></script>
 		<script type="text/javascript" src="js/jquery.maskMoney.js"></script>
-		<script type="text/javascript" src="js/bootstrap.js"></script>
-		
-		
-		<script type="text/javascript" src="js/sweetalert.min.js"></script>
+
 
 	</head>
 	<body>
@@ -40,7 +40,7 @@
 			<div class="row">
 		        <div class="col-lg-12">
 		          
-		        	<form id="formRegraTres" method="post" action="#">
+		        	<form id="formRegraTres" method="post" action="#" class="form-inline">
 
 
 		        		<div class="row">
@@ -48,8 +48,8 @@
 		        			<div class="col-md-4">
 			        			<label >Unidade primaria
 	 							
-	 								<select id="medidaPrimaria">
-	 									<option value=" " >Selecione...</option>
+	 								<select id="medidaPrimaria" name="medidaPrimaria" class="form-control ">
+	 									<option value="" >Selecione...</option>
 	 									<option value="1">Metros</option>
 	 									<option value="2">Quilos</option>
 	 									<option value="3">Litros</option>
@@ -62,8 +62,8 @@
 
 		 						<label >Unidade secundaria
 	 							
-	 								<select id="medidaPrimaria">
-	 									<option value=" " >Selecione...</option>
+	 								<select id="medidaSecundaria" name="medidaSecundaria" class="form-control">
+	 									<option value="" >Selecione...</option>
 	 									<option value="1">Metros</option>
 	 									<option value="2">Quilos</option>
 	 									<option value="3">Litros</option>
@@ -75,6 +75,10 @@
 
 		 					<div class="col-md-4">
 
+		 						<label > Dobro / Metade
+
+		 						</labeL>
+
 		 					</div>
 		        		</div>
 
@@ -83,7 +87,7 @@
 		        			<div class="col-md-4">
 			        			<label >Número A 
 	 							
-	 								<input id="numeroA" type="text" name="numeroA" >
+	 								<input id="numeroA" type="text" name="numeroA" class="form-control ">
 
 		 						<label>
 		 					</div>
@@ -92,7 +96,7 @@
 
 		 						<label> Número B
 
-		 							<input id="numeroB" type="text" name="numeroB" >
+		 							<input id="numeroB" type="text" name="numeroB" class="form-control ">
 
 	 							</label>
 
@@ -102,6 +106,7 @@
 
 	 							<label > 
 	 							
+	 								<input type="checkbox" id="grandeza" name="grandeza" style="height:32px;" checked>
 
 		 						<label>
 
@@ -115,7 +120,7 @@
 		        			<div class="col-md-4">
 			        			<label >Número C
 	 							
-	 								<input id="numeroC" type="text" name="numeroC" >
+	 								<input id="numeroC" type="text" name="numeroC" class="form-control">
 
 		 						<label>
 		 					</div>
@@ -124,7 +129,7 @@
 
 		 						<label> Número D
 
-		 							<input id="numeroD" type="text" name="numeroD" disabled>
+		 							<input id="numeroD" type="text" name="numeroD" class="form-control" disabled>
 
 	 							</label>
 
@@ -134,7 +139,7 @@
 
 	 							<label >
 	 								
-	 								<button id="btnGerarResultados" class="btn btn-default" type="submit">Calcular</button>
+	 								<button id="btnGerarResultados" class="btn btn-primary" type="submit">Calcular</button>
 
 		 						<label>
 
@@ -161,14 +166,80 @@
 	</body>
 </html>
 
-<script type="text/javascript">
-
+<script type="text/javascript"> 
+ 
 	$(function(){
-		$('#numeroA').mask('?99999');
-		$('#numeroB').mask('?99999');
-		$('#numeroC').mask('?99999');
+
+		var options = {
+		    onText: "Diminui",
+		    onColor: 'primary',
+		    offColor: 'danger',
+		    offText: "Aumentar",
+		    animate: true,
+		};
+
+		$("#grandeza").bootstrapSwitch(options);
+
+		$('#numeroA').mask('?9999999999999999');
+		$('#numeroB').mask('?9999999999999999');
+		$('#numeroC').mask('?9999999999999999');
+
+		$("#grandeza").on('change',function(){
+
+			console.log("Ok amiguinho!")
+
+		});
 
 
+		$('#medidaPrimaria').on('change',function(){
+			
+			var medpri 	= $('#medidaPrimaria').val();
+
+			switch(medpri){
+				case '1' :
+					$('#numeroA').mask('?999999999999999');
+					$('#numeroC').mask('?999999999999999');
+				break;
+				case '2' :
+					$('#numeroA').mask('?99999999999,999');
+					$('#numeroC').mask('?99999999999,999');
+				break;
+				case '3' :
+					$('#numeroA').mask('?99999999999,999');
+					$('#numeroC').mask('?99999999999,999');
+				break;
+				case '4':
+					$("#numeroA").maskMoney({decimal:",", thousands:""});
+					$("#numeroC").maskMoney({decimal:",", thousands:""});
+				break;
+
+			}
+
+		});
+
+		$('#medidaSecundaria').on('change',function(){
+
+			var medsec 	= $('#medidaSecundaria').val();
+
+			switch(medpri){
+				case '1' :
+					$('#numeroB').mask('?999999999999999');		
+				break;
+				case '2' :
+					$('#numeroB').mask('?99999999999,999');
+					
+				break;
+				case '3' :
+					$('#numeroB').mask('?99999999999,999');
+					
+				break;
+				case '4':
+					$("#numeroB").maskMoney({decimal:",", thousands:""});
+				break;
+			}
+
+
+		});
 
 		$('#btnGerarResultados').click(function(e){
 
@@ -177,28 +248,38 @@
 			// VALIDAÇÃO DO FORMULÁRIO DE CADASTRO
 			$('#formRegraTres').validate({
 		        rules:{
-					numeroA: {
+					numeroA:{
 						required: true
 					},
-					numeroB: {
-						required: true,
-						
+					numeroB:{
+						required: true
 					},
-					numeroC: {
-						required: true,
+					numeroC:{
+						required: true
+					},
+					medidaPrimaria:{
+						required: true
+					},
+					medidaSecundaria:{
+						required: true
 					}
 		        },
 		        messages:{
-		        	numeroA: {
+		        	numeroA:{
 		        		required: "Campo obrigatório."
 		        	},
-		        	numeroB: {
-		        		required: "Campo obrigatório.",
-		        		
+		        	numeroB:{
+		        		required: "Campo obrigatório."	
 		        	},
-		        	numeroC: {
-		        		required: "Campo obrigatório.",
-		        	}
+		        	numeroC:{
+		        		required: "Campo obrigatório."
+		        	},
+		        	medidaPrimaria:{
+						required: "Campo obrigatório."
+					},
+					medidaSecundaria:{
+						required: "Campo obrigatório."
+					}
 		        }
 		    });
 
